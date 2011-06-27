2cb18d0481e4cacdf3ca9f00cc3a64121a8bad22
Greg Combs <gcombs@gmail.com>
2011-06-27 16:50:46 -0700

    Edited license

--- IntelligentSplitViewController.h
+++ IntelligentSplitViewController.h
@@ -2,7 +2,7 @@
 //  IntelligentSplitViewController.h
 //  From TexLege by Gregory S. Combs (last updated 9/21/10)
 //
-//  Released under the Creative Commons Attribution-ShareAlide 3.0 Unported License
+//  Released under the Creative Commons Attribution 3.0 Unported License
 //  Please see the included license page for more information.
 //  In a nutshell, you can use this, just attribute this class to me in your thank you notes or about box.
 
